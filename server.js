ded891770258e27a948f25fb711392afce4cf513
S2- <s2@p.r0xy.it>
2013-02-20 10:43:08 +0100

    drop root priviledges after port 53 binded.

--- server.js
+++ server.js
@@ -4,19 +4,55 @@ var app = express();
 var server = named.createServer();
 
 var ip = '127.0.0.1';	//default ip when nothing is registered yet
+var process_user = 'nobody';
+var process_group = 'nogroup';
+
+
+var httpInterface = function() {
+	//http interface
+	app.get('/register', function(req, res){
+		ip = req.get('x-forwarded-for');
+		res.send('ok: ' + ip);
+		console.log('registered ip ' + ip);
+	});
+
+	app.get('/status', function(req, res){
+	        res.send(ip);
+	});
+
+	app.listen(22023, '127.0.0.1');
+	console.log('Listening on port 22023');
+}
+
 
 
 server.listen(53, '0.0.0.0', function() {
-        console.log('DNS server started on port 53');
+	console.log('DNS server started on port 53');
+        
+	try {
+		console.log('Giving up root privileges...');
+		process.setgid(process_group);
+		process.setuid(process_user);
+		console.log('New uid: ' + process.getuid());
+		
+		httpInterface();
+	}
+	catch (err) {
+		console.log('Failed to drop root privileges: ' + err);
+	}
 });
 
 server.on('query', function(query) {
-        var domain = query.name()
-        var type = query.type();
-        console.log('DNS Query: (%s) %s', type, domain);
+	var domain = query.name()
+	var type = query.type();
+	
+	console.log('DNS Query: (%s) %s', type, domain);
 
-	var record = new named.ARecord(ip);
-	query.addAnswer(domain, record, 5);
+	if (type === 'A') {
+		var record = new named.ARecord(ip);
+		query.addAnswer(domain, record, 5);
+	}
+	
 	server.send(query);
 
 });
@@ -30,17 +66,5 @@ server.on('uncaughtException', function(error) {
 });
 
 
-//http interface
-app.get('/register', function(req, res){
-	ip = req.ip;
-	res.send('ok: ' + ip);
-	console.log('registered ip ' + ip);
-});
 
-app.get('/status', function(req, res){
-        res.send(ip);
-});
-
-app.listen(22023);
-console.log('Listening on port 22023');
 
